fabbb18056e24786b9d871fc705822d713228d38
JulyKikuAkita <b92701105@gmail.com>
2022-04-23 23:17:55 -0700

    try to add a third view layer

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,10 +7,32 @@
 
 import SwiftUI
 
+/*
+ SwiftUI restrains:
+ Detail views always get a navigation bar whether you want it or not, so you need to use navigationBarHidden(true) to hide it.
+ There’s no way of making the primary view stay visible even when there is more than enough space.
+ You can’t make the primary view shown in landscape by default; SwiftUI always chooses the detail.
+
+ */
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            NavigationLink {
+                Text("New Secondary")
+            } label: {
+                Text("Hello World")
+            }
+            .navigationTitle("Primary")
+
+            NavigationLink {
+                Text("New Third")
+            } label: {
+                Text("Hello World Secondary")
+            }
+            .navigationTitle("Secondary")
+
+            Text("Third View")
+        }
     }
 }
 
