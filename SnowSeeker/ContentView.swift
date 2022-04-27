192f0034fa06a51f584a3eed3443fb3c2219635f
JulyKikuAkita <b92701105@gmail.com>
2022-04-26 23:21:52 -0700

    Day 96: Making a SwiftUI view searchable

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            // need to make sure your view is inside a NavigationView, otherwise iOS won’t have anywhere to put the search box
+            // iOS will automatically hide the search bar at the very top of the list – you’ll need to pull down gently to reveal it
+            List(filteredNames, id: \.self) { name in
+                Text("\(name)")
+
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationBarTitle("Searching")
+        }
+    }
+
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            return allNames
+        } else {
+            return allNames.filter {$0.localizedCaseInsensitiveContains(searchText)}
+        }
     }
 }
 
