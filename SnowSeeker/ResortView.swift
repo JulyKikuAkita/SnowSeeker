c342bf733414daba1f53a9fb6a79109e26eacc41
JulyKikuAkita <b92701105@gmail.com>
2022-04-28 22:53:03 -0700

    Day 98: Changing a view’s layout in response to size classes

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -7,8 +7,19 @@
 
 import SwiftUI
 
+/*
+ SizeClass:
+ All iPhones in portrait have compact width and regular height.
+ Most iPhones in landscape have compact width and compact height.
+ Large iPhones (Plus-sized and Max devices) in landscape have regular width and compact height.
+ All iPads in both orientations have regular width and regular height when your app is running with the full screen.
+
+ // if need to limit dynamic type size, using .dynamicTypeSize(...DynamicTypeSize.xxxLarge) make xxxLarge the upper bound
+ */
 struct ResortView: View {
     let resort: Resort
+    @Environment(\.horizontalSizeClass) var sizeClass
+    @Environment(\.dynamicTypeSize) var typeSize
 
     var body: some View {
         ScrollView {
@@ -18,8 +29,13 @@ struct ResortView: View {
                     .scaledToFit()
 
                 HStack {
-                    ResortDetailsView(resort: resort)
-                    SkiDetailsView(resort: resort)
+                    if sizeClass == .compact && typeSize > .large {
+                        VStack(spacing: 10) { ResortDetailsView(resort: resort) }
+                        VStack(spacing: 10) { SkiDetailsView(resort: resort) }
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                    }
                 }
                 .padding(.vertical)
                 .background(Color.primary.opacity(0.1))
@@ -45,5 +61,6 @@ struct ResortView: View {
 struct ResortView_Previews: PreviewProvider {
     static var previews: some View {
         ResortView(resort:  Resort.example)
+            .previewInterfaceOrientation(.portrait)
     }
 }
